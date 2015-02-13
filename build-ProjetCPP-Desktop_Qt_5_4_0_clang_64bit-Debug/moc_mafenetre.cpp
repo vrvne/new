bb1c9b6ef76d4464031bd6ca16420e1331c4586b
vrvne <junjing@JJs-laptop.local>
2015-02-13 17:19:53 +0100

    build

---
+++ build-ProjetCPP-Desktop_Qt_5_4_0_clang_64bit-Debug/moc_mafenetre.cpp
@@ -0,0 +1,131 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mafenetre.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../ProjetCPP/mafenetre.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mafenetre.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MaFenetre_t {
+    QByteArrayData data[8];
+    char stringdata[59];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MaFenetre_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MaFenetre_t qt_meta_stringdata_MaFenetre = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "MaFenetre"
+QT_MOC_LITERAL(1, 10, 7), // "message"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 7), // "save_St"
+QT_MOC_LITERAL(4, 27, 6), // "save_K"
+QT_MOC_LITERAL(5, 34, 6), // "save_r"
+QT_MOC_LITERAL(6, 41, 10), // "save_sigma"
+QT_MOC_LITERAL(7, 52, 6) // "save_T"
+
+    },
+    "MaFenetre\0message\0\0save_St\0save_K\0"
+    "save_r\0save_sigma\0save_T"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MaFenetre[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x0a /* Public */,
+       3,    0,   45,    2, 0x0a /* Public */,
+       4,    0,   46,    2, 0x0a /* Public */,
+       5,    0,   47,    2, 0x0a /* Public */,
+       6,    0,   48,    2, 0x0a /* Public */,
+       7,    0,   49,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MaFenetre::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MaFenetre *_t = static_cast<MaFenetre *>(_o);
+        switch (_id) {
+        case 0: _t->message(); break;
+        case 1: _t->save_St(); break;
+        case 2: _t->save_K(); break;
+        case 3: _t->save_r(); break;
+        case 4: _t->save_sigma(); break;
+        case 5: _t->save_T(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MaFenetre::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_MaFenetre.data,
+      qt_meta_data_MaFenetre,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MaFenetre::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MaFenetre::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MaFenetre.stringdata))
+        return static_cast<void*>(const_cast< MaFenetre*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int MaFenetre::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
